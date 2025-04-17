205bea8ba420d692371106421ff4ff1818b2053a
Draco Zhang <1092469607@qq.com>
2025-04-17 13:21:41 +1000

    Added local schedule initialising from a global schedule

--- src/gttcan.c
+++ src/gttcan.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "gttcan.h"
 
-#define REFERENCE_FRAME_DATA_ID 0
-#define GENERIC_DATA_ID 1 
 
 // GTTCAN INIT
 // This function should initialise the local schedule
@@ -10,6 +8,7 @@
 void gttcan_init(
     gttcan_t *gttcan,
     uint8_t node_id,
+    global_schedule_ptr_t global_schedule_ptr,
     uint16_t global_schedule_length,
     uint32_t slot_duration,
     transmit_frame_callback_fp_t transmit_frame_callback_fp,
@@ -19,46 +18,12 @@ void gttcan_init(
     gttcan->node_id = node_id;
     gttcan->global_schedule_length = global_schedule_length;
     gttcan->slot_duration = slot_duration;
+    gttcan->local_schedule_index = 0;
+
+    gttcan_get_local_schedule(gttcan,global_schedule_ptr);
 
     gttcan->transmit_frame_callback_fp = transmit_frame_callback_fp;
     gttcan->set_timer_int_callback_fp = set_timer_int_callback_fp;
-
-    gttcan->local_schedule_index = 0;
-
-    gttcan->local_schedule[0].slot_id = 0;
-    gttcan->local_schedule[0].data_id = REFERENCE_FRAME_DATA_ID;
-
-    gttcan->local_schedule[1].slot_id = 1;
-    gttcan->local_schedule[1].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[2].slot_id = 2;
-    gttcan->local_schedule[2].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[3].slot_id = 3;
-    gttcan->local_schedule[3].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[4].slot_id = 5;
-    gttcan->local_schedule[4].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[5].slot_id = 8;
-    gttcan->local_schedule[5].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[6].slot_id = 13;
-    gttcan->local_schedule[6].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[7].slot_id = 21;
-    gttcan->local_schedule[7].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[8].slot_id = 34;
-    gttcan->local_schedule[8].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[9].slot_id = 55;
-    gttcan->local_schedule[9].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule[10].slot_id = 89;
-    gttcan->local_schedule[10].data_id = GENERIC_DATA_ID;
-
-    gttcan->local_schedule_length = 11;
 
 }
 
@@ -121,6 +86,19 @@ void gttcan_start(
     gttcan_transmit_next_frame(gttcan);
 
 
+}
+
+
+void gttcan_get_local_schedule(gttcan_t * gttcan, global_schedule_ptr_t global_schedule_ptr){
+    uint8_t local_schedule_index = 0;
+    for (int i = 0; i < gttcan->global_schedule_length; i++) {
+        if (global_schedule_ptr[i].node_id == gttcan->node_id) {
+            gttcan->local_schedule[local_schedule_index].slot_id = global_schedule_ptr[i].slot_id;
+            gttcan->local_schedule[local_schedule_index].data_id = global_schedule_ptr[i].data_id;
+            local_schedule_index++;
+        }
+    }
+    gttcan->local_schedule_length = local_schedule_index;
 }
 
 
--- src/include/gttcan.h
+++ src/include/gttcan.h
@@ -23,12 +23,31 @@
 #define GTTCAN_NUM_DATA_ID_BITS 16
 #endif
 
+#define REFERENCE_FRAME_DATA_ID 0 //ifndef?
+#define GENERIC_DATA_ID 1  //ifndef?
+
 // Consider making this into masked 29 bit CAN Frame IDs from start, but this struct is more readable
 // I think NUM_SLOT_ID_BITS and NUM_DATA_ID_BITS could be up to the end user, but is there any point???
 typedef struct local_schedule_entry_tag { // According to claude has no memory overhead vs a uint32_t, but will be one more operation when it comes time to transmit a frame
     uint16_t slot_id; // could be as large as GTTCAN_MAX_GLOBAL_SCHEDULE_LENGTH
-    uint16_t data_id; // could be as large as the size of the whiteboard
+    uint16_t data_id; // could be as large as the size of the whiteboard //TODO:?
 } local_schedule_entry_t; // THIS SHOULD BE USED when it needs to be used in a user defined function 
+
+
+
+#define MAX_GLOBAL_SCHEDULE_LENGTH 120 // check if number is ok and ifndef
+
+typedef struct global_schedule_entry{
+    uint8_t node_id;
+    uint16_t slot_id;
+    uint16_t data_id; // didn't use local_schedule_entry_tag didn't want to over-complicate it
+} global_schedule_entry_t;
+
+typedef global_schedule_entry_t* global_schedule_ptr_t;
+
+
+
+
 
 typedef void (*transmit_frame_callback_fp_t)(uint32_t, uint64_t);
 typedef void (*set_timer_int_callback_fp_t)(uint32_t);
@@ -38,6 +57,7 @@ typedef struct gttcan_tag {
 
     // Schedule related
     local_schedule_entry_t local_schedule[GTTCAN_MAX_LOCAL_SCHEDULE_LENGTH];
+    // global_schedule_ptr_t global_schedule_ptr; //could keep a pointer in the future if we need to use global_schedule, i.e. fault tolerance or if global_schedule needs to be dynamically changed?
     uint16_t global_schedule_length;
     uint8_t local_schedule_length;
     uint8_t local_schedule_index;
@@ -53,6 +73,7 @@ typedef struct gttcan_tag {
 void gttcan_init(
     gttcan_t *gttcan,
     uint8_t node_id,
+    global_schedule_ptr_t global_schedule_ptr,
     uint16_t global_schedule_length,
     uint32_t slot_duration,
     transmit_frame_callback_fp_t transmit_frame_callback_fp,
@@ -64,5 +85,7 @@ void gttcan_start(
 );
 
 void gttcan_transmit_next_frame(gttcan_t * gttcan);
+
+void gttcan_get_local_schedule(gttcan_t * gttcan, global_schedule_ptr_t global_schedule_ptr);
 
 uint16_t gttcan_get_number_of_slots_to_next(uint16_t current_slot_id, uint16_t next_slot_id, uint16_t global_schedule_length);
